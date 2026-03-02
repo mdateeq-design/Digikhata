35649d903889c5e06b2c646089618dd394c78469
afrozhussain1998 <afrozhussain1998@gmail.com>
2026-03-02 08:03:11 +0530

    DB Updated!

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log('MongoDB Connected: ${conn.connection.host}');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error('Error connecting to MongoDB: ${err.message}');
+    console.error(`Error connecting to MongoDB: ${err.message}`);
     process.exit(1);
   }
 };
